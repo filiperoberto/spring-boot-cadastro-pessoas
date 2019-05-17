65e5196e95598baef83d61d2df09466867dc5236
Filipe Silva <frsilva@ciasc.sc.gov.br>
2019-05-17 13:29:32 -0300

    Initial commit

---
+++ src/main/java/com/example/cadastro/domain/Pessoa.java
@@ -0,0 +1,13 @@
+package com.example.cadastro.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Pessoa {
+
+    private int id;
+    private String nome;
+
+}
